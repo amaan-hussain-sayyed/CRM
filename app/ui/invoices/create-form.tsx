0a88fcb576ec00af22e9d51d32b28150776434e8
sayyed amaan husaain <121231641+amaan-hussain-sayyed@users.noreply.github.com>
2024-07-10 03:37:10 +0530

    Update create-form.tsx

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -11,7 +11,7 @@ import { Button } from '@/app/ui/button';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="rounded-md text-white p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -21,7 +21,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <select
               id="customer"
               name="customerId"
-              className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              className="peer block w-full cursor-pointer rounded-md  py-2 pl-10 text-sm outline-2 placeholder:text-gray-600"
               defaultValue=""
             >
               <option value="" disabled>
