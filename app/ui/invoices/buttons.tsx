a9cc12ad7df5041f24cae77ff67b1d779fd1cb56
sayyed amaan husaain <121231641+amaan-hussain-sayyed@users.noreply.github.com>
2024-07-12 02:33:25 +0530

    Update buttons.tsx

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,7 +16,7 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      href="/dashboard/Invoices"
       className="rounded-md  p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
