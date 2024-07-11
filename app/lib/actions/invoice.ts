51970df6f434782b8a8108cdacc7f9f148d7eb14
sayyed amaan husaain <121231641+amaan-hussain-sayyed@users.noreply.github.com>
2024-07-12 02:47:18 +0530

    Update invoice.ts

--- app/lib/actions/invoice.ts
+++ app/lib/actions/invoice.ts
@@ -13,7 +13,7 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
-const createInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -40,7 +40,7 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/Invoices');
 }
 
-
+const UpdateInvoice = FromSchema()
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
