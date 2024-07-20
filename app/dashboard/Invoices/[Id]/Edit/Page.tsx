355779121d94de8d66f5d9cf702ad2eb9bc26061
sayyed amaan husaain <121231641+amaan-hussain-sayyed@users.noreply.github.com>
2024-07-21 03:53:33 +0530

    Update Page.tsx

--- app/dashboard/Invoices/[Id]/Edit/Page.tsx
+++ app/dashboard/Invoices/[Id]/Edit/Page.tsx
@@ -1,1 +1,21 @@
-
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/Invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/Invoices/${id}/Edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
