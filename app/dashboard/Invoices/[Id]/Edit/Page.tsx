cabf7a47dec9763fdc12b235920b1afdc97053ac
sayyed amaan husaain <121231641+amaan-hussain-sayyed@users.noreply.github.com>
2024-07-25 01:22:42 +0530

    Update Page.tsx

--- app/dashboard/Invoices/[Id]/Edit/Page.tsx
+++ app/dashboard/Invoices/[Id]/Edit/Page.tsx
@@ -1,9 +1,13 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById ,fetchCustomers } from '@/app/lib/data';
  
 export default async function Page({param}:{Id:string}) {
  const id = param.Id
+ const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
   return (
     <main>
       <Breadcrumbs
